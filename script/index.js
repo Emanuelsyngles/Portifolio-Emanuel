6567e2aa83c3b35063cfc1f5c80336baca8f778b
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-03-11 17:12:48 -0300

    Montando outras funcionalidades

--- script/index.js
+++ script/index.js
@@ -1,6 +1,12 @@
 let body = document.querySelector("#body")
 let frase = document.querySelector('.frase_robo_1')
 let audio = document.getElementById('audioplayer')
+let audiotchau = document.querySelector('#audiodespedida')
+let musica = document.querySelector('#musicamenu')
+let musica2 = document.querySelector('#musicamenu2')
+let apresentacao = document.querySelector('.apresentacao')
+
+
 function claro() {
     body.style.background = '#014984'
 }
@@ -12,8 +18,39 @@ function escuro() {
 function visual() {
     frase.style.display ='block'
     audio.play()
+    musica.play()
 }
 
 function removeroboot() {
     frase.style.display = 'none'
+    audiotchau.play()
 }
+
+audiotchau.addEventListener('play', function() {
+    if(!audio.pause()) {
+        audio.pause()
+        musica.pause()
+    }
+})
+
+function ajustarvolume() {
+    musica.volume = 0.1
+}
+
+function desligar() {
+    musica.pause()
+}
+
+function tour() {
+    frase.style.display = 'none'
+    apresentacao.style.display = 'block'
+    musica2.play()
+}
+
+musica2.addEventListener('play', function() {
+    if(!musica.pause()) {
+        musica.pause()
+    }
+})
+
+
